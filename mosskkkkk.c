5b8c97d604267e1ff33651e5f3d7c327215f1cf6
kid3302 <minhduy250802@gmail.com>
2024-03-06 17:38:10 +0700

    sửa 6/6

--- mosskkkkk.c
+++ mosskkkkk.c
@@ -2,29 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+
 struct nguoichoi
 {
     char ten[24];
     int solandoan;
 };
-void inrakyluc(struct nguoichoi* player,int dem)
+
+void inrakyluc(struct nguoichoi* player, int dem)
 {
-    for (int i=0;i<dem;i++)
+    for (int i = 0; i < dem; i++)
     {
-        printf("kỷ lục %d *****\n tên người choi:%s \n số lần đoán %d",i,player[i].ten,player[i].solandoan);
+        printf("kỷ lục %d *****\n tên người choi:%s \n số lần đoán %d\n", i+1, player[i].ten, player[i].solandoan);
     }
 }
+
 void swap(struct nguoichoi *a, struct nguoichoi *b) {
     struct nguoichoi temp = *a;
     *a = *b;
     *b = temp;
 }
+
 int taosongaunhien()
 {
-     srand(time(NULL));
+    srand(time(NULL));
     int randomNumber = rand() % 9000 + 1000; 
     return randomNumber;
 }
+
 void sosanhketqua(int number[], int *arr, int *arrrd) {
     for (int i = 0; i < 4; i++) 
     { 
@@ -34,6 +39,7 @@ void sosanhketqua(int number[], int *arr, int *arrrd) {
         }
     }
 }
+
 void hamsapxep(struct nguoichoi* playerds, int dem) {
     for(int i = 0; i < dem - 1; i++) {
         for(int j = 0; j < dem - 1; j++) {
@@ -43,6 +49,7 @@ void hamsapxep(struct nguoichoi* playerds, int dem) {
         }
     }
 }
+
 void tachSo(int numberrandom, int *arr) 
 {
     arr[0] = numberrandom / 1000;        
@@ -50,113 +57,71 @@ void tachSo(int numberrandom, int *arr)
     arr[2] = (numberrandom / 10) % 10;   
     arr[3] = numberrandom % 10;          
 }
+
 void luuvaofile(struct nguoichoi player)
 {
-    struct nguoichoi playerds[5];
-    FILE *file = fopen("danhsach.txt", "r+");
+    FILE *file = fopen("danhsach.txt", "a+");
     if (file == NULL) {
         printf("Không thể mở tệp tin.\n");
         exit(1);
     }
-    int demmoi=0;
-    int dem=0;
-    while (fread(playerds, sizeof(struct nguoichoi), 1, file) == 1) 
-    {
-      dem++;
-    }
-    if(dem<5)
-    {
-        fseek(file, 0, SEEK_END);
-        fwrite(&player,sizeof(struct nguoichoi),1,file);
-         fseek(file, 0, SEEK_SET);
-         hamsapxep(playerds,dem);
-    }
-    else if(dem=5)
-    {
-        while (fread(playerds, sizeof(struct nguoichoi), 1, file) == 1) 
-            {
-            }
-                  hamsapxep(playerds,5);
-
-    for(int i=0;i<5;i++)
-    {
-        if(player.solandoan>playerds[i].solandoan && player.solandoan<playerds[i+1].solandoan)
-        {
-            playerds[i+1]=player;
-            i++;
-            for (int k = 3; k >= i; k--) 
-            {
-                    playerds[k + 1] = playerds[k];
-            }
-        }
-        else if(player.solandoan=playerds[i].solandoan )
-        {
-            playerds[i+1]=player;
-            i++;
-            for (int k = i; k <5; k++)
-{
-                    playerds[k + 1] = playerds[k];
-            }
-        }
-    }
-    
-    }
-inrakyluc(playerds,dem);
- fclose(file);
+    fclose(file);
 }
-void inraketqua(int* arr,int* arrrd,int* dem)
+
+
+  void inraketqua(int* arr, int* arrrd, int* dem)
 {
-     while (arr[0] == -1 || arr[1] == -1 || arr[2] == -1 || arr[3] == -1) 
-     { 
-       nhap:
+while (arr[0] == -1 || arr[1] == -1 || arr[2] == -1 || arr[3] == -1)
+    {
+        nhap:
         printf("Nhập 4 số bạn chọn để đoán (0-9): ");
         int number[4];
-        for (int i=0;i<4;i++)
+        for (int i = 0; i < 4; i++)
         {
             scanf("%d", &number[i]);
-            if(number[i]>9 || number[i]<0)
+            if (number[i] > 9 || number[i] < 0)
             {
-             printf("giá trị nhập không hợp lệ\n");
+                printf("giá trị nhập không hợp lệ\n");
                 goto nhap;
             }
-            (*dem)++;
         }
-        sosanhketqua(&number, arr, arrrd);
-        for (int i = 0; i < 4; i++) 
+        (*dem)++; // Cập nhật số lần đoán
+        sosanhketqua(number, arr, arrrd);
+        for (int i = 0; i < 4; i++)
         {
-            if (arr[i] != -1) 
+            if (arr[i] != -1)
             {
                 printf("%d", arr[i]);
-            } 
+            }
             else
             {
                 printf("_");
             }
         }
         printf("\n");
-        printf("số lần bạn đã đoán:%d\n",*dem);
-        if(number!=arrrd[0] && number!=arrrd[1] && number!=arrrd[2] && number!=arrrd[3]  )
-        printf("nhập sai vui lòng nhập lại\n");
+        printf("số lần bạn đã đoán:%d\n", *dem);
+        if (number[0] != arrrd[0] && number[1] != arrrd[1] && number[2] != arrrd[2] && number[3] != arrrd[3])
+            printf("nhập sai vui lòng nhập lại\n");
     }
 }
+
 int main() {
     char name[24];
-    int dem=0;
-    int* demptr=&dem;
-    int randomNumber=taosongaunhien();
-  
+    int dem = 0;
+    int* demptr = &dem;
+    int randomNumber = taosongaunhien();
+
     int arrrd[4];
     tachSo(randomNumber, arrrd);
-     printf("Số ngẫu nhiên là: %d\n", randomNumber);
-     float sacxuat=sacxuatcuabaitoan(arrrd);
-    printf("sác xuất bài toán  là: %f %\n", sacxuat*100);
+    printf("Số ngẫu nhiên là: %d\n", randomNumber);
     int arr[4] = {-1, -1, -1, -1}; 
     printf("Vui lòng nhập tên của bạn:");
     fgets(name, sizeof(name), stdin);
-    inraketqua(arr,arrrd,demptr);
+    
+    inraketqua(arr, arrrd, &dem);
     struct nguoichoi nguoichoi1;
-    strcpy(nguoichoi1.ten,name);
-    nguoichoi1.solandoan=dem;
+    strcpy(nguoichoi1.ten, name);
+    nguoichoi1.solandoan = dem;
     luuvaofile(nguoichoi1);
     return 0;
 }
